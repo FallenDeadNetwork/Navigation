93062f28a61c6375b987b7b794f5ffaff514fe50
Rark7040 <crimera.hello@gmail.com>
2022-01-10 16:10:16 +0900

    座標表示のorderを追加

--- src/navi/order/obj/Coordinate.php
+++ src/navi/order/obj/Coordinate.php
@@ -3,25 +3,20 @@ declare(strict_types=1);
 
 namespace navi\order\obj;
 
-use pocketmine\world\Position as PMPosition;
+use pocketmine\math\Vector2;
 
-class Position{
-	protected PMPosition $position;
+class Coordinate extends Vector2{
 	protected string $name;
 	protected ?float $send_distance;
 
-	public function __construct(PMPosition $position, string $name, ?float $distance = null){
-		$this->position = $position;
+	public function __construct(float $x, float $y, string $name, ?float $distance = null){
+		parent::__construct($x, $y);
 		$this->name = $name;
 		$this->send_distance = $distance;
 	}
 
 	public function getName():string{
 		return $this->name;
-	}
-
-	public function getPosition():PMPosition{
-		return $this->position;
 	}
 
 	public function getSendDistance():?float{
